e70c285ba87508469020dc87703e074becfb4083
Mike O'Neal <MikeONeal18@gmail.com>
2019-09-12 22:41:03 +0000

    Quiz 2

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -2,4 +2,8 @@ class QuizController < ApplicationController
     def def index
         
     end
+
+    def simple_form_for
+        @simple_form= action
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'quiz#index'
+  resources :choose do
+    resources :pictures, only: :create
+      
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
